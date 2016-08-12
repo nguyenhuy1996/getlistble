025154eaef13d18168377bb59fe4cb1deffa3cfd
ADMIN <admin@admins-macbook-4.local>
2016-08-12 09:07:37 +0700

    Initial Commit

---
+++ getlistble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getlistble
+//
+//  Created by admin on 12/08/2016.
+//  Copyright © 2016 HITDEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
